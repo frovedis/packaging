2797a8254eebb0d28c90e05d439d97de7a8320f1
Takuya Araki <t-araki@dc.jp.nec.com>
2018-07-10 17:08:31 +0900

    avoid wget if the files are already downloaded

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 . ./x86env.sh
 cd ./tgz
-wget `cat openmpi-3.0.0.tar.gz.URL`
-wget `cat scala-2.11.12.tgz.URL`
-wget `cat spark-2.2.1-bin-hadoop2.7.tgz.URL`
+if [ ! -f openmpi-3.0.0.tar.gz ]; then
+	wget `cat openmpi-3.0.0.tar.gz.URL`
+fi
+if [ ! -f scala-2.11.12.tgz ]; then
+	wget `cat scala-2.11.12.tgz.URL`
+fi
+if [ ! -f spark-2.2.1-bin-hadoop2.7.tgz ]; then
+	wget `cat spark-2.2.1-bin-hadoop2.7.tgz.URL`
+fi
 cd ../build
 tar -zxf ../tgz/openmpi-3.0.0.tar.gz
 cd openmpi-3.0.0
