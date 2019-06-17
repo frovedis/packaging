2e6051c0f2a035f5d87c8a1a243fc97251e02cd8
Takuya Araki <t-araki@dc.jp.nec.com>
2019-06-17 19:43:59 +0900

    modify MANPATH not to contain same paths

--- veenv.sh
+++ veenv.sh
@@ -8,6 +8,10 @@ ldpath_append ()  { ldpath_remove $1; export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$
 ldpath_prepend () { ldpath_remove $1; export LD_LIBRARY_PATH="$1:$LD_LIBRARY_PATH"; }
 ldpath_remove ()  { export LD_LIBRARY_PATH=`echo -n $LD_LIBRARY_PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
 
+manpath_append ()  { manpath_remove $1; export MANPATH="$MANPATH:$1"; }
+manpath_prepend () { manpath_remove $1; export MANPATH="$1:$MANPATH"; }
+manpath_remove ()  { export MANPATH=`echo -n $MANPATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
+
 export INSTALLPATH=/opt/nec/nosupport/frovedis
 export X86_INSTALLPATH=${INSTALLPATH}/x86
 export VE_INSTALLPATH=${INSTALLPATH}/ve
@@ -26,4 +30,5 @@ export SPARK_HOME=${X86_INSTALLPATH}/opt/${SPARK}
 export SPARK_SUBMIT_OPTIONS="--driver-java-options \"-Djava.library.path=${X86_INSTALLPATH}/lib\" --jars ${X86_INSTALLPATH}/lib/spark/frovedis_client.jar --conf spark.driver.memory=8g"
 export PYTHONPATH=${X86_INSTALLPATH}/lib/python
 ldpath_append ${X86_INSTALLPATH}/lib
-export MANPATH=`manpath -q`:${VE_INSTALLPATH}/man
+export MANPATH=`manpath -q`
+manpath_append ${VE_INSTALLPATH}/man
