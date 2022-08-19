6c4f5698d4bb7d4b3d3b43fa073900089aee8730
Takuya Araki <takuya_araki@nec.com>
2022-08-19 21:56:45 +0900

    add explicit null string as the argument of necmpivars.sh to avoid warning when called from shell script

--- veenv.sh
+++ veenv.sh
@@ -14,7 +14,7 @@ manpath_remove ()  { export MANPATH=`echo -n $MANPATH | awk -v RS=: -v ORS=: '$0
 
 if [ -z "$NMPI_ROOT" ]; then
 	MPIVAR=`ls /opt/nec/ve/mpi | grep ^[0-9] | sort -V -r | head -1`
-	source /opt/nec/ve/mpi/${MPIVAR}/bin/necmpivars.sh
+	source /opt/nec/ve/mpi/${MPIVAR}/bin/necmpivars.sh ""
 fi
 export INSTALLPATH=/opt/nec/frovedis
 export X86_INSTALLPATH=${INSTALLPATH}/x86
