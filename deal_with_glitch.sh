d35f7d068b3261af7c141f271fa52a38adbef8da
Takuya Araki <takuya_araki@nec.com>
2024-03-29 13:50:43 +0900

    rm -fcheck-noexcpt-violation; mv boost related part to boost-ve

--- deal_with_glitch.sh
+++ deal_with_glitch.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # modify files to deal with the glitch of nc++ 5.2.0
-# run before do_everything.sh
 
 set -eu
 
-sed s@-O3\ \;@-O3\ -fcheck-noexcept-violation\ -fno-cse-after-vectorization\ \;@g -i ../boost-ve/modified/tools/build/src/tools/ncc.jam
-
-sed s@-finline-max-function-size=200\ -Wno-unknown-pragma@-finline-max-function-size=200\ -fcheck-noexcept-violation\ -fno-cse-after-vectorization\ -Wno-unknown-pragma@g -i ../ve/Makefile.in.ve
+sed s@-finline-max-function-size=200\ -Wno-unknown-pragma@-finline-max-function-size=200\ -fno-cse-after-vectorization\ -Wno-unknown-pragma@g -i ../ve/Makefile.in.ve
 
 sed 452s@\#if\ defined\(__GNUC__\)\ \|\|\ defined\(__clang__\)@\#if\ 0@g -i ../ve/third_party/cereal-1.2.2/include/cereal/external/rapidjson/rapidjson.h
 sed 465s@\#if\ defined\(__GNUC__\)\ \|\|\ defined\(__clang__\)@\#if\ 0@g -i ../ve/third_party/cereal-1.2.2/include/cereal/external/rapidjson/rapidjson.h
